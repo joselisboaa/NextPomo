0eb66a4887fa5446ff272df87de0e2dcd4ffb9f5
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-18 17:57:34 -0300

    styles: timer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Logo } from "@/assets";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex h-[3.5rem] w-full flex-row items-center justify-between bg-primary">
+    <header className="flex h-3.5 w-full flex-row items-center justify-between bg-primary">
       <div className="flex flex-row-reverse items-center justify-between gap-2 pl-6 pr-6 sm:flex-row">
         <Image src={Logo} alt="HealthPomo Logo" width={24} height={24} />
         <p className="text-sm font-medium text-white">
--- src/features/pomodoro/components/timer/index.tsx
+++ src/features/pomodoro/components/timer/index.tsx
@@ -1,7 +1,19 @@
+import classNames from "classnames";
+import { twMerge } from "tailwind-merge";
+
 export const Timer = () => {
   return (
-    <div className="my-5 flex h-[18.75rem] w-[18.75rem] items-center justify-center rounded-full border-2 border-solid border-secondary">
-      <span className="text-white">25:00</span>
+    <div
+      className={twMerge(
+        classNames(
+          "my-5 flex h-[18.75rem] w-[18.75rem] items-center justify-center rounded-full border-2 border-solid border-secondary",
+          {
+            "border-rest-primary": false,
+          },
+        ),
+      )}
+    >
+      <span className="font-Roboto text-[6.25rem] font-bold text-white">25:00</span>
     </div>
   );
 };
