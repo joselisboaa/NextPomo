612600b1eb34a1f709266da93e6342c693fb7603
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-18 17:39:54 -0300

    styles: pomodoro main page

--- src/features/pomodoro/components/pomoNav/index.tsx
+++ src/features/pomodoro/components/pomoNav/index.tsx
@@ -4,20 +4,16 @@ import { twMerge } from "tailwind-merge";
 
 export const PomodoroNav = () => {
   return (
-    <div
-      className={twMerge(
-        "flex h-[3.5rem] w-40 items-center justify-center gap-2 rounded-full bg-primary md:h-[3.5rem]",
-      )}
-    >
-      <button className="flex w-[8.75rem] gap-2 text-white ">
+    <div className={twMerge("flex h-3.5 w-fit items-center justify-center rounded-full bg-primary md:h-[3.5rem]")}>
+      <button className="flex h-full w-fit items-center justify-center gap-2 rounded-l-[2rem] px-4 text-white hover:bg-hover-primary">
         <Image src={Pomodoro} alt="Pomodoro Starter" />
         <span>Pomodoro</span>
       </button>
-      <button className="flex w-[8.75rem] gap-2 text-white">
+      <button className="flex h-full w-fit items-center justify-center gap-2 px-4 text-white hover:bg-hover-primary">
         <Image src={ShortRestIcon} alt="Short rest" />
         <span>Pausa curta</span>
       </button>
-      <button className="flex w-[8.75rem] gap-2 text-white">
+      <button className="flex h-full w-fit items-center justify-center gap-2 rounded-r-[2rem] px-4 text-white hover:bg-hover-primary">
         <Image src={LongRestIcon} alt="Long rest" />
         <span>Pausa longa</span>
       </button>
--- src/features/pomodoro/components/timer/index.tsx
+++ src/features/pomodoro/components/timer/index.tsx
@@ -1,7 +1,7 @@
 export const Timer = () => {
   return (
-    <div className="my-5 flex h-[18.75] w-[18.75rem] items-center justify-center rounded-full">
-      <span>25:00</span>
+    <div className="my-5 flex h-[18.75rem] w-[18.75rem] items-center justify-center rounded-full border-2 border-solid border-secondary">
+      <span className="text-white">25:00</span>
     </div>
   );
 };
