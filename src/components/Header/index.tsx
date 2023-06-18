783728d2dcefe7ed40fbc4768fb0b3b8630f77f1
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-18 14:03:06 -0300

    styles: header title and background

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,12 @@ import { Logo } from "@/assets";
 const Header: React.FC = () => {
   return (
     <header className="flex h-[3.5rem] w-full flex-row items-center justify-between bg-primary py-5">
-      <div className="flex h-20 flex-row-reverse items-center justify-between pl-6 pr-6 sm:flex-row">
+      <div className="flex h-20 gap-2 flex-row-reverse items-center justify-between pl-6 pr-6 sm:flex-row">
         <Image src={Logo} alt="HealthPomo Logo" width={24} height={24} />
+        <p className="text-sm font-medium text-white">
+          Health
+          <span className="text-sm font-medium text-secondary">Pomo</span>
+        </p>
       </div>
     </header>
   );
