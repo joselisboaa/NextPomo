ba75708fcbbcb68bb0e281ba6b77b3483eb320d8
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-19 16:33:36 -0300

    styles: change flex-row-reverse to flex flow

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { Logo } from "@/assets";
 const Header: React.FC = () => {
   return (
     <header className="flex h-3.5 w-full flex-row items-center justify-between bg-primary">
-      <div className="flex flex-row-reverse items-center justify-between gap-2 pl-6 pr-6 sm:flex-row">
+      <div className="flex flex-row items-center justify-between gap-2 pl-6 pr-6">
         <Image src={Logo} alt="HealthPomo Logo" width={24} height={24} />
         <p className="text-sm font-medium text-white">
           Health
