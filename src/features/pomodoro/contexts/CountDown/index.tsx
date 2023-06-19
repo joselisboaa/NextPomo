ba6f57184b43895dd8e1d78fd7cb4faa5526527b
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-18 22:25:21 -0300

    feat: timer context

---
+++ src/features/pomodoro/contexts/CountDown/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Dispatch, SetStateAction, createContext, useState } from "react";
+import { useTimeLeft } from "../../hooks/useTimeLeft";
+
+interface ICountDown {
+  isActive: boolean;
+  isRestTime: boolean;
+  hasFinished: boolean;
+  setIsPaused: Dispatch<SetStateAction<boolean>>;
+  setIsRestTime: Dispatch<SetStateAction<boolean>>;
+  seconds: number;
+  minutes: number;
+}
+
+interface Provider {
+  children: React.ReactNode;
+}
+
+export const CountDownContext = createContext({} as ICountDown);
+
+export const CountDownProvider = ({ children }: Provider) => {
+  const [isRestTime, setIsRestTime] = useState<boolean>(false);
+  const [isPaused, setIsPaused] = useState<boolean>(true);
+  const normalBreak = 25 * 60;
+
+  const standardTime = useTimeLeft(normalBreak, isPaused);
+
+  const minutes = standardTime.minutes;
+  const seconds = standardTime.seconds;
+
+  const hasFinished = minutes + seconds === 0;
+  const isActive = minutes + seconds > 0;
+
+  return (
+    <CountDownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        hasFinished,
+        isRestTime,
+        isActive,
+        setIsPaused,
+        setIsRestTime,
+      }}
+    >
+      {children}
+    </CountDownContext.Provider>
+  );
+};
