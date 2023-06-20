12dda25a86252b8cd049d64077708c49b8fc40d3
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-19 23:11:58 -0300

    fix: put HealthPomo logo and title inside a container

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,14 @@ import { Logo } from "@/assets";
 const Header: React.FC = () => {
   return (
     <header className="flex h-3.5 w-full flex-row items-center justify-between bg-primary">
-      <div className="flex flex-row items-center justify-between gap-2 pl-6 pr-6">
-        <Image src={Logo} alt="HealthPomo Logo" width={24} height={24} />
-        <p className="text-sm font-medium text-white">
-          Health
-          <span className="text-sm font-medium text-secondary">Pomo</span>
-        </p>
+      <div className="flex w-full flex-row items-center justify-between gap-2 px-6">
+        <div className="flex items-center justify-between gap-2">
+          <Image src={Logo} alt="HealthPomo Logo" width={24} height={24} />
+          <p className="text-sm font-medium text-white">
+            Health
+            <span className="text-sm font-medium text-secondary">Pomo</span>
+          </p>
+        </div>
       </div>
     </header>
   );
